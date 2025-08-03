22115368c6ed1abe3bc6814481485f7aa795f55c
PlatiniumYT <johnringot6259@email.com>
2025-08-03 17:26:44 +0200

    Mise à jour roulette: bouton aléatoire + roulette interactive

--- src/App.js
+++ src/App.js
@@ -424,11 +424,9 @@ function App() {
           marginBottom: 18,
           display: "flex",
           justifyContent: "center",
-          paddingLeft: 16,
-          paddingRight: 16
         }}
       >
-        <div className="roulette-tapis-fr" style={{ display: "flex", alignItems: "flex-start", minWidth: 700 }}>
+        <div className="roulette-tapis-fr" style={{ display: "flex", alignItems: "flex-start", minWidth: 1000		}}>
           <div
             className={`case zero green${isSelected(0) ? " selected" : ""} ${highlightNumbers.includes(0) ? "highlight" : ""}`}
             style={{ width: 42, height: 42, marginRight: 10, fontWeight: 900, fontSize: "1.13em" }}
